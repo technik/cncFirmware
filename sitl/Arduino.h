3d6c34bf157ac079557496f941e9cae58a95f2aa
Carmelo Fernandez Aguera <carferna@microsoft.com>
2021-02-24 15:41:05 -0800

    Playing around with steppers.

--- sitl/Arduino.h
+++ sitl/Arduino.h
@@ -1,9 +1,11 @@
 // Mock arduino interface
 #pragma once
+#include <iostream>
 
 struct SerialComm
 {
 	void begin(unsigned baudrate) {}
+	void write(unsigned x) { std::cout << x << "\n"; }
 
 	static SerialComm com0;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,234 +1,183 @@
 #include <Arduino.h>
 #include <hal/gpio_pin.h>
-#include <coroutine>
 #include <staticRingBuffer.h>
 #include <chrono>
+#include <utility>
+#include <hal/boards/arduinomega2560.h>
 
 using namespace etl::hal;
 using namespace etl;
 
-namespace nes::ted {}
+using clock = std::chrono::steady_clock;
+using time_point = clock::time_point;
+using duration = clock::duration;
 
-struct Promise;
-
-struct Task
+template<class StepPin, class DirPin, class EnablePin>
+struct StepperDriver
 {
-	using promise_type = Promise;
+	void enable() { enablePin.setLow(); }
+	void disable() { enablePin.setHigh(); }
 
-	Task(Promise& promise) : m_promise(promise) {}
-
-	constexpr bool await_ready() const noexcept { return false; }
-	void await_suspend(std::coroutine_handle<Promise> h);
-	void await_resume();
-
-	void then(std::coroutine_handle<> post);
-
-	Promise& m_promise;
-};
-
-struct Promise
-{
-	auto initial_suspend() noexcept { return std::suspend_never{}; }
-	auto final_suspend() noexcept { return std::suspend_never{}; }
-	Task get_return_object() noexcept { return Task{ *this }; }
-	void return_void()
+	void setDir(bool sign)
 	{
-		m_post.resume();
-	}
-	void unhandled_exception() const noexcept {}
-
-	template<class Awaitable>
-	Awaitable await_transform(Awaitable expr)
-	{
-		expr.then(m_handle); // Append my own execution at the end of the awaitable expression
-		return expr;
+		if(sign)
+			dirPin.setHigh();
+		else
+			dirPin.setLow();
 	}
 
-	std::coroutine_handle<Promise> m_handle;
-	std::coroutine_handle<> m_post;
-};
-
-void Task::await_suspend(std::coroutine_handle<Promise> h)
-{
-	m_promise.m_handle = h;
-}
-
-void Task::await_resume()
-{
-	m_promise.m_handle.resume();
-}
-
-void Task::then(std::coroutine_handle<> post)
-{
-	m_promise.m_post = post;
-}
-
-template<class T>
-struct TimedPromise;
-
-template<class T>
-struct TimedTask
-{
-	using promise_type = TimedPromise<T>;
-};
-
-template<class T>
-struct TimedPromise
-{	
-	using Clock = std::chrono::steady_clock;
-
-	Clock::time_point due;
-
-	// Promise interface
-	auto initial_suspend() { return std::suspend_always{}; }
-	auto final_suspend() { return std::suspend_never{}; }
-	void return_value(T) {}
-	TimedTask<T> get_return_object() { return {}; }
-};
-
-class TaskScheduler
-{
-public:
-	static TaskScheduler& Get() {
-		static TaskScheduler singleton;
-		return singleton;
+	void step()
+	{
+		stepPin.setHigh();
+		delayMicroseconds(20);
+		stepPin.setLow();
 	}
 
-	static constexpr size_t kMaxRealTimeTasks = 16;
-	static constexpr size_t kMaxPriorityTasks = 8;
-	static constexpr size_t kMaxBackgroundTasks = 8;
-	// Task queues
-	//FixedRingBuffer<Task, kMaxRealTimeTasks> 	realTimeQueue;
-	//FixedRingBuffer<Task, kMaxPriorityTasks> 	priorityQueue;
-	//FixedRingBuffer<Task, kMaxBackgroundTasks> 	backgroundQueue;
+	StepPin::Out stepPin;
+	DirPin::Out dirPin;
+	EnablePin::Out enablePin;
+};
 
-	void Continue()
+// Ramps 1.4 definitions
+using XAxisStepper = StepperDriver<Pin54,Pin55,Pin38>;
+// using YAxisStepper = StepperDriver<Pin60,Pin61,Pin56>; // Original RAMPS mapping
+using YAxisStepper = StepperDriver<Pin26,Pin28,Pin24>; // Remapping due to a few burnt traces
+using ZAxisStepper = StepperDriver<Pin46,Pin48,Pin62>;
+
+XAxisStepper gMotorX;
+YAxisStepper gMotorY;
+ZAxisStepper gMotorZ;
+
+struct StepperController
+{
+	bool Finished();
+	bool Continue();
+
+	enum class Axis {
+		X, Y, Z
+	};
+
+	void setStepDir(bool sign);
+	void resetPosition();
+	void configMicroStepping();
+
+	struct StepperState
 	{
-	/*	if(!realTimeQueue.empty())
+		duration stepPeriod;
+		int32_t pendingSteps;
+		int32_t position;
+	};
+};
+
+template<class Pin>
+struct InputButton
+{
+	void read()
+	{
+		m_state = (m_state<<1) & FullStateMask;
+		m_state |= m_pin;
+	}
+	bool pressed() const { return m_state == CurStateMask; }
+	bool held() const { return m_state == FullStateMask; }
+	bool released() const { return m_state == LastStateMask; };
+
+private:
+	static constexpr uint8_t CurStateMask = 1;
+	static constexpr uint8_t LastStateMask = (1<<1);
+	static constexpr uint8_t FullStateMask = CurStateMask | LastStateMask;
+	// bit 0: current state; bit 1: past state
+	uint8_t m_state{};
+	Pin::In m_pin;
+};
+
+template<int pinX, int pinY, class ButtonPin>
+struct AnalogJoystick
+{
+	struct Axis
+	{
+		Axis(int pin)
+			: m_pin(pin)
 		{
-			realTimeQueue.pop_front();
-			return;
-		}*/
-	}
-};
+			pinMode(pin, INPUT);
+			m_center = analogRead(pin);
+		}
 
-template<class Clock_>
-class TimeoutPromise;
+		void read()
+		{
+			m_pos = analogRead(m_pin);
+		}
 
-template<class Clock_>
-class AwaitableTimeout
-{
-public:
-	using time_point = typename Clock_::time_point;
-	using promise_type = TimeoutPromise<Clock_>;
+		int m_pin;
+		int m_pos;
+		uint16_t m_center;
+		uint16_t m_max;
+		uint16_t m_min;
+	};
 
-	AwaitableTimeout(time_point t) noexcept : m_t(t) {}
-
-	// Note: Instead of always shceduling the task, we could check here whether we passed the time already.
-	// However, periodic tasks are usually scheduled well in advance, so it's not very useful in practice.
-	constexpr bool await_ready() const noexcept { return false; }
-
-	// Awaiter interface
-	void await_suspend(std::coroutine_handle<> h) {
-		m_coHandle = h;
-		// Add h to the scheduler so it resume us;
-	}
-	void await_resume() {}
-	void then(std::coroutine_handle<> postOp) { m_post = postOp; }
-private:
-	time_point m_t;
-	std::coroutine_handle<> m_coHandle;
-	std::coroutine_handle<> m_post;
-};
-
-template<class Clock_>
-class TimeoutPromise
-{
-public:
-	using Clock = Clock_;
-	using time_point = typename Clock::time_point;
-
-	TimeoutPromise(time_point t)
-		: m_awaiter(t)
-	{}
-
-	// Promise interface
-	auto initial_suspend()
+	void read()
 	{
-		return std::suspend_always{};
+		xAxis.read();
+		yAxis.read();
+		button.read();
 	}
-	auto final_suspend() { return std::suspend_never{}; }
-	AwaitableTimeout<Clock_> get_return_object() { return m_awaiter; }
 
-	void return_void() {}
-
-private:
-	AwaitableTimeout<Clock_> m_awaiter;
+	Axis xAxis{pinX};
+	Axis yAxis{pinY};
+	InputButton<ButtonPin> button;
 };
 
-// suspend execution until the given time point is reached
-template<class Clock_>
-AwaitableTimeout<Clock_> asyncTimeout(std::chrono::time_point<Clock_> t) { co_return; }
+LedPin gLed;
 
-Task driveStepper()
-{
-	constexpr auto dt = std::chrono::microseconds(1000);
-	auto tNext = std::chrono::steady_clock::now();
-	for(;;)
-	{
-		tNext += dt;
-		co_await asyncTimeout(tNext);
-	}
-}
-// Async code to drive a stepper
-// dt = getMotorDt();
-// co_await asyncTimeout(lastTime + dt);
-// stepPulse();
+using XMinEndStop = Pin3::In;
 
-// G-Code parsing loop
-// {
-//		for(;;)
-//			line = co_await serial.getLine();
-//			opCode = parseLine(line);
-//			co_yield opCode;
-// }
-
-// opcode execution loop
-// Task<> executeGCode();
-// {
-	//	for(;;)
-	//		op = co_await GCodeParse();
-	//		// Invoke op from a call table
-	//		operation[op]();
-// }
-
-// motorControl();
-// {
-// 		SetupMotors();
-//		res = RunDiagnostics();
-//		if(res)
-		// stepperX.run();
-		// stepperY.run();
-		// ...
-// }
+XMinEndStop gEndStopMinX;
 
 void setup() {
 	// Setup scheduler
 	// Setup serial port
-	Serial.begin(115200);
+	Serial.begin(9600);
 	// Start motor control
-	// runSteppers();
+	gMotorY.setDir(false);
+	gMotorX.enable();
+	gMotorY.enable();
+	gMotorZ.enable();
 	// Start message processing
 	// executeGCode();
 }
 
+AnalogJoystick<A5, A10, Pin44> gLeftStick;
+
 void loop() {
+	
+	/*delay(1000);
+	Serial.print("X:");
+	Serial.print(gLeftStick.xAxis.m_pos);
+	Serial.print(" Y:");
+	Serial.println(gLeftStick.yAxis.m_pos);
+	*/
+	if(gLeftStick.xAxis.m_pos < 60)
+	{
+		gLed.setHigh();
+		gMotorX.setDir(true);
+	}
+	else
+	{
+		gLed.setLow();
+		gMotorX.setDir(false);
+	}
+
+	gLeftStick.read();
+	gMotorX.step();
+	//gMotorY.step();
+	//gMotorZ.step();
+	delay(1);
+	if(gEndStopMinX)
+		gMotorY.setDir(true);
   // Consume work from the task queue
   // Global work flow looks like:
   // - Interrupt from hardware stops
   // - Interrupt from motor control
   // - Consume data from the serial port. Interpret the G-Code and write ack to it
-  TaskScheduler::Get().Continue();
 }
 
 #ifdef _WIN32
